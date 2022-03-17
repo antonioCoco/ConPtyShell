4ce4a1ab61ed3a7ee972bbe00f1a5f344d0fd7be
antonioCoco <a.cocomazzi93@gmail.com>
2022-03-17 23:46:07 +0100

    dropped support for alternative powershell wrapper

--- base64_conversion_commands.ps1
+++
@@ -1,3 +0,0 @@
-$Filename = (Get-Location).Path + "\ConPtyShell_dotnet2.exe"
-$base64string_x64 = [Convert]::ToBase64String([IO.File]::ReadAllBytes($FileName))
-$base64string_x64 | Out-File ConPtyShell.base64
